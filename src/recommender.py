28b652962d2c834f7507305f37e1a4719737e32c
gurvanbadrakh <hhatnaa690@gmail.com>
2026-03-15 02:10:21 +0800

    Add movie recommendation function
    Implement movie recommendation function using correlation.

--- src/recommender.py
+++ src/recommender.py
@@ -1,1 +1,32 @@
+import pandas as pd
 
+movies = pd.read_csv("data/raw/movies.csv")
+ratings = pd.read_csv("data/raw/ratings.csv")
+
+movie_ratings = pd.merge(ratings, movies, on="movieId")
+
+movie_matrix = movie_ratings.pivot_table(
+    index="userId",
+    columns="title",
+    values="rating"
+)
+
+def recommend(movie_title):
+
+    movie_ratings = movie_matrix[movie_title]
+
+    similar = movie_matrix.corrwith(movie_ratings)
+
+    corr_df = pd.DataFrame(similar, columns=["correlation"])
+
+    corr_df.dropna(inplace=True)
+
+    rating_count = movie_ratings.groupby("title")["rating"].count()
+
+    corr_df["rating_count"] = rating_count
+
+    recommendations = corr_df[corr_df["rating_count"] > 100] \
+        .sort_values("correlation", ascending=False) \
+        .head(10)
+
+    return recommendations
