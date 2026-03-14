852355c8311e76602ba26ca4e1c7e289be1d3c65
gurvanbadrakh <hhatnaa690@gmail.com>
2026-03-15 02:14:25 +0800

    Refactor recommender to use ratings with titles

--- src/recommender.py
+++ src/recommender.py
@@ -3,26 +3,23 @@ import pandas as pd
 movies = pd.read_csv("data/raw/movies.csv")
 ratings = pd.read_csv("data/raw/ratings.csv")
 
-movie_ratings = pd.merge(ratings, movies, on="movieId")
+ratings_with_titles = pd.merge(ratings, movies, on="movieId")
 
-movie_matrix = movie_ratings.pivot_table(
+movie_matrix = ratings_with_titles.pivot_table(
     index="userId",
     columns="title",
     values="rating"
 )
 
 def recommend(movie_title):
+    target_ratings = movie_matrix[movie_title]
 
-    movie_ratings = movie_matrix[movie_title]
-
-    similar = movie_matrix.corrwith(movie_ratings)
+    similar = movie_matrix.corrwith(target_ratings)
 
     corr_df = pd.DataFrame(similar, columns=["correlation"])
-
     corr_df.dropna(inplace=True)
 
-    rating_count = movie_ratings.groupby("title")["rating"].count()
-
+    rating_count = ratings_with_titles.groupby("title")["rating"].count()
     corr_df["rating_count"] = rating_count
 
     recommendations = corr_df[corr_df["rating_count"] > 100] \
